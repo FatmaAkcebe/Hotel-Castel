33faa967c3a7b7b25932b50fb5466e94793b6312
MuhammetYazici <yzcmm61@gmail.com>
2025-03-13 15:23:07 +0300

    Anfahrt baslangıc

---
+++ src/test/java/featureFiles/Check if the hotel has directions.feature
@@ -0,0 +1,5 @@
+Feature: Check if the hotel has directions
+
+  Scenario: directions to the hotel
+    Given Navigate to hootelCastle
+    When Check the description of the hotel's location.
---
+++ src/test/java/stepDefinitions/CheckIfTheHotelHasDirections.java
@@ -0,0 +1,21 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import pages.DialogContent;
+import utilities.GWD;
+
+public class CheckIfTheHotelHasDirections {
+
+    DialogContent dc = new DialogContent();
+
+    @Given("Navigate to hootelCastle")
+    public void navigateToHootelCastle() {
+        GWD.getDriver().get("https://hotel-castle-rastatt.de/");
+        dc.myClick(dc.anfahrt);
+    }
+
+    @When("Check the description of the hotel's location.")
+    public void checkTheDescriptionOfTheHotelSLocation() {
+    }
+}
