9d7ee91469724392d6e4123210cd846e1aecd72b
SefaKahramannn <sefakahraman0001@gmail.com>
2025-04-07 22:51:28 +0300

    Project Updated

--- src/test/java/runner/SmokeTest.java
+++ src/test/java/runner/SmokeTest.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        tags = "@RegressionTest or @SmokeTest",
+        tags = "@SmokeTest",
         features = {"src/test/java/featureFiles"},
         glue = {"stepDefinitions", "hooks"}
 )
